5e9abdf940c0d108533a7f6e8d47e85d9d3728bf
nguyenhanh52889 <nguyenhanh.52889@gmail.com>
2017-12-05 03:33:29 +0700

    Update solve.go

--- task5/solve.go
+++ task5/solve.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
+    "encoding/json"
+    "log"
+    "net/http"
 
-	"github.com/justincampbell/url-shortener-go/urlstore"
+    "github.com/couchbase/gocb"
+    "github.com/gorilla/mux"
 )
 
-var (
-	port     = flag.String("port", "8082", "port to listen on")
-	urlStore = urlstore.NewURLStore()
-)
+type MyUrl struct {
+    ID       string `json:"id,omitempty"`
+    LongUrl  string `json:"longUrl,omitempty"`
+    ShortUrl string `json:"shortUrl,omitempty"`
+}
 
-func init() {
-	flag.Parse()
+var bucket *gocb.Bucket
+var bucketName string
+
+func ExpandEndpoint(w http.ResponseWriter, req *http.Request) {
+    var n1qlParams []interface{}
+    query := gocb.NewN1qlQuery("SELECT `" + bucketName + "`.* FROM `" + bucketName + "` WHERE shortUrl = $1")
+    params := req.URL.Query()
+    n1qlParams = append(n1qlParams, params.Get("shortUrl"))
+    rows, _ := bucket.ExecuteN1qlQuery(query, n1qlParams)
+    var row MyUrl
+    rows.One(&row)
+    json.NewEncoder(w).Encode(row)
+}
+
+
+func RootEndpoint(w http.ResponseWriter, req *http.Request) {
+    params := mux.Vars(req)
+    var url MyUrl
+    bucket.Get(params["id"], &url)
+    http.Redirect(w, req, url.LongUrl, 301)
 }
 
 func main() {
-	http.HandleFunc("/", expandHandler)
-
-	fmt.Println("Listening on", *port)
-	err := http.ListenAndServe(":"+*port, nil)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func expandHandler(response http.ResponseWriter, request *http.Request) {
-	fmt.Println(request.Method, request.RequestURI, request.RemoteAddr)
-
-	if request.RequestURI == "/" {
-		url := "http://github.com/justincampbell/url-shorteners"
-		http.Redirect(response, request, url, 301)
-		return
-	}
-
-	token := request.URL.Path[len("/"):]
-	url := urlStore.Expand(token)
-
-	if url == "" {
-		http.NotFound(response, request)
-		return
-	}
-
-	http.Redirect(response, request, url, 301)
+    router := mux.NewRouter()
+    cluster, _ := gocb.Connect("couchbase://localhost")
+    bucketName = "example"
+    bucket, _ = cluster.OpenBucket(bucketName, "")
+    router.HandleFunc("/{id}", RootEndpoint).Methods("GET")
+    router.HandleFunc("/expand/", ExpandEndpoint).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8082", router))
 }
