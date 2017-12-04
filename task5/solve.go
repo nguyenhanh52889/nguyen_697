a2ba7698137deebbf24c2b60566102707d3f1d58
nguyenhanh52889 <nguyenhanh.52889@gmail.com>
2017-12-05 03:24:23 +0700

    Update solve.go

--- task5/solve.go
+++ task5/solve.go
@@ -1,77 +1,48 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "time"
+	"flag"
+	"fmt"
+	"net/http"
 
-    "github.com/couchbase/gocb"
-    "github.com/gorilla/mux"
-    "github.com/speps/go-hashids"
+	"github.com/justincampbell/url-shortener-go/urlstore"
 )
 
-type MyUrl struct {
-    ID       string `json:"id,omitempty"`
-    LongUrl  string `json:"longUrl,omitempty"`
-    ShortUrl string `json:"shortUrl,omitempty"`
-}
+var (
+	port     = flag.String("port", "8082", "port to listen on")
+	urlStore = urlstore.NewURLStore()
+)
 
-var bucket *gocb.Bucket
-var bucketName string
-
-func ExpandEndpoint(w http.ResponseWriter, req *http.Request) {
-    var n1qlParams []interface{}
-    query := gocb.NewN1qlQuery("SELECT `" + bucketName + "`.* FROM `" + bucketName + "` WHERE shortUrl = $1")
-    params := req.URL.Query()
-    n1qlParams = append(n1qlParams, params.Get("shortUrl"))
-    rows, _ := bucket.ExecuteN1qlQuery(query, n1qlParams)
-    var row MyUrl
-    rows.One(&row)
-    json.NewEncoder(w).Encode(row)
-}
-
-func CreateEndpoint(w http.ResponseWriter, req *http.Request) {
-    var url MyUrl
-    _ = json.NewDecoder(req.Body).Decode(&url)
-    var n1qlParams []interface{}
-    n1qlParams = append(n1qlParams, url.LongUrl)
-    query := gocb.NewN1qlQuery("SELECT `" + bucketName + "`.* FROM `" + bucketName + "` WHERE longUrl = $1")
-    rows, err := bucket.ExecuteN1qlQuery(query, n1qlParams)
-    if err != nil {
-        w.WriteHeader(401)
-        w.Write([]byte(err.Error()))
-        return
-    }
-    var row MyUrl
-    rows.One(&row)
-    if row == (MyUrl{}) {
-        hd := hashids.NewData()
-        h := hashids.NewWithData(hd)
-        now := time.Now()
-        url.ID, _ = h.Encode([]int{int(now.Unix())})
-        url.ShortUrl = "http://localhost:8082/" + url.ID
-        bucket.Insert(url.ID, url, 0)
-    } else {
-        url = row
-    }
-    json.NewEncoder(w).Encode(url)
-}
-
-func RootEndpoint(w http.ResponseWriter, req *http.Request) {
-    params := mux.Vars(req)
-    var url MyUrl
-    bucket.Get(params["id"], &url)
-    http.Redirect(w, req, url.LongUrl, 301)
+func init() {
+	flag.Parse()
 }
 
 func main() {
-    router := mux.NewRouter()
-    cluster, _ := gocb.Connect("couchbase://localhost")
-    bucketName = "example"
-    bucket, _ = cluster.OpenBucket(bucketName, "")
-    router.HandleFunc("/{id}", RootEndpoint).Methods("GET")
-    router.HandleFunc("/expand/", ExpandEndpoint).Methods("GET")
-    router.HandleFunc("/create", CreateEndpoint).Methods("PUT")
-    log.Fatal(http.ListenAndServe(":8082", router))
+	http.HandleFunc("/", expandHandler)
+
+	fmt.Println("Listening on", *port)
+	err := http.ListenAndServe(":"+*port, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func expandHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Println(request.Method, request.RequestURI, request.RemoteAddr)
+
+	if request.RequestURI == "/" {
+		url := "http://github.com/justincampbell/url-shorteners"
+		http.Redirect(response, request, url, 301)
+		return
+	}
+
+	token := request.URL.Path[len("/"):]
+	url := urlStore.Expand(token)
+
+	if url == "" {
+		http.NotFound(response, request)
+		return
+	}
+
+	http.Redirect(response, request, url, 301)
 }
